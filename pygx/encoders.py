1c65ea04173280246a0dae888a6d64bb899135a9
sebaspv <poncegramming@gmail.com>
2024-03-30 22:48:18 -0600

    mol generation

--- pygx/encoders.py
+++ pygx/encoders.py
@@ -18,11 +18,9 @@ class VGCNEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(VGCNEncoder, self).__init__()
         self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=False)
-        self.conv2 = GCNConv(2 * out_channels, 2 * out_channels, cached=False)
         self.mu = GCNConv(2 * out_channels, out_channels, cached=False)
         self.logstd = GCNConv(2 * out_channels, out_channels, cached=False)
 
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index)
-        x = self.conv2(x, edge_index).relu()
+        x = self.conv1(x, edge_index).relu()
         return self.mu(x, edge_index), self.logstd(x, edge_index)
--- pygx/gvae.py
+++ pygx/gvae.py
@@ -1,18 +1,19 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
-from torch_geometric.datasets import Planetoid
+import torch.nn.functional as F
+from torch_geometric.datasets import ZINC
 from torch_geometric.nn import VGAE
 import torch_geometric.transforms as T
 from torch_geometric.utils import train_test_split_edges
 from encoders import VGCNEncoder
 
-dataset = Planetoid("data/", "CiteSeer", transform=T.NormalizeFeatures())
+dataset = ZINC("data/", subset=True)
 data = dataset[0]
 data.train_mask = data.val_mask = data.test_mask = data.y = None
 data = train_test_split_edges(data)
 writer = SummaryWriter("runs/VGAE_experiment_" + "2d_100_epochs")
 
-out_channels = 2
+out_channels = 16
 n_features = data.num_features
 epochs = 300
 
@@ -20,9 +21,10 @@ model = VGAE(VGCNEncoder(n_features, out_channels))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
+data.x = F.normalize(data.x.float())
 x = data.x.to(device)
 train_pos_edge_index = data.train_pos_edge_index.to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
 def train():
@@ -37,16 +39,16 @@ def train():
     return float(loss)
 
 
+@torch.no_grad()
 def test(pos_edge_index, neg_edge_index):
     model.eval()
-    with torch.no_grad():
-        z = model.encode(x, train_pos_edge_index)
+    z = model.encode(x, train_pos_edge_index)
     return model.test(z, pos_edge_index, neg_edge_index)
 
 
 for epoch in range(1, epochs + 1):
     loss = train()
     auc, ap = test(data.test_pos_edge_index, data.test_neg_edge_index)
-    print(f"Epoch: {epoch}, AUC: {auc}, AP: {ap}")
-    writer.add_scalar('auc train',auc,epoch) # new line
-    writer.add_scalar('ap train',ap,epoch)   # new line
+    writer.add_scalar("auc train", auc, epoch)
+    writer.add_scalar("ap train", ap, epoch)
+    print(f"Epoch: {epoch} AUC: {auc} AP: {ap}")
